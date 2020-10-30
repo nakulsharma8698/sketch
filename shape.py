ace9b87561ae2e5df0d9c04b8e55e54f147b56f7
Nakul Sharma <nakulsharma8698@gmail.com>
2020-10-30 14:10:22 +0530

    First Commit

---
+++ shape.py
@@ -0,0 +1,33 @@
+import argparse
+import imutils
+import cv2
+# construct the argument parse and parse the arguments
+#ap = argparse.ArgumentParser()
+#ap.add_argument("-i", "C:/Users/nakul/Desktop/NAKUL/shape.jpg", required=True,
+#	help="path to the input image")
+#args = vars(ap.parse_args())
+# load the image, convert it to grayscale, blur it slightly,
+# and threshold it
+image = cv2.imread("C:/Users/nakul/Desktop/NAKUL/cir.jpeg")
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
+cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+for c in cnts:
+	M = cv2.moments(c)
+	if M["m00"] != 0:
+		cX = int(M["m10"] / M["m00"])
+		cY = int(M["m01"] / M["m00"])
+	else:
+		cX, cY = 0, 0
+
+
+	cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
+	cv2.circle(image, (cX, cY), 7, (255, 255, 255), -1)
+	cv2.putText(image, "center", (cX - 20, cY - 20),
+		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+	cv2.imshow("Image", thresh)
+	cv2.waitKey(0)
+
