b417c686523e15242bbf64694759687e624bc319
Nakul Sharma <nakulsharma8698@gmail.com>
2020-11-01 14:47:03 +0530

    First Commit

---
+++ pyimagesearch/coordinate.py
@@ -0,0 +1,23 @@
+import cv2
+from pyimagesearch.points import Points
+class Coordinate:
+    def __init__(self):
+        pass
+    def coordinates(self, c):
+        approx = cv2.approxPolyDP(c, 0.009 * cv2.arcLength(c, True), True)
+        n = approx.ravel()
+        i = 0
+        val = Points()
+        for j in n:
+            if (i % 2 == 0):
+                x = n[i]
+                y = n[i + 1]
+
+                # String containing the co-ordinates.
+                string = str(x) + " " + str(y)
+
+                if (i == 0):
+                    # text on topmost co-ordinate.
+                    coo = val.first(x,y)
+            
+        return coo
